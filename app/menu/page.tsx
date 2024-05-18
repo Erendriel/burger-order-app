00b2e3cb6460cd3ac7529e708df96ea7b8e4006f
Edgar Eli Perez-Figueroa y Larrañaga <edgar_eli12@hotmail.com>
2024-05-18 14:25:00 -0600

    feat: add menu ui

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,7 +1,50 @@
+import { product } from "@/data";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const MenuPage = () => {
-  return <div>MenuPage</div>;
+  return (
+    <div className="flex flex-wrap justify-center gap-4">
+      {product.map((item) => (
+        <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/4 p-4" key={item.id}>
+          <Link
+            className="block rounded-lg shadow-lg overflow-hidden transform transition duration-300 hover:scale-105 group"
+            href={`/product/${item.id}`}
+          >
+            {/* IMAGE CONTAINER */}
+            {item.image && (
+              <div className="relative h-72">
+                <Image
+                  src={item.image}
+                  alt=""
+                  layout="fill"
+                  className="object-cover"
+                />
+              </div>
+            )}
+            {/* TEXT CONTAINER */}
+            <div className="p-4 bg-white h-48 group-hover:bg-fuchsia-50 transition-colors duration-300 flex flex-col justify-between">
+              <div>
+                <h1 className="text-xl font-bold uppercase mb-2">
+                  {item.name}
+                </h1>
+                <p className="text-sm mb-4">{item.description}</p>
+              </div>
+              <div className="flex items-center justify-between mt-auto mb-2">
+                <h2 className="text-lg font-bold text-red-500">
+                  ${(item.price / 100).toFixed(2)}
+                </h2>
+                <button className="ml-2 uppercase bg-red-500 text-white p-2 rounded-md">
+                  Add to Cart
+                </button>
+              </div>
+            </div>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default MenuPage;
