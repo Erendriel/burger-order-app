9fa5b1728e262f6dfe168cc94c806dc7bbba893f
Edgar Eli Perez-Figueroa y Larrañaga <edgar_eli12@hotmail.com>
2024-05-18 15:16:40 -0600

    feat: Add SingleProduct page, add quantity functionality

--- components/Price.tsx
+++ components/Price.tsx
@@ -2,42 +2,22 @@
 
 import React, { useEffect, useState } from "react";
 
-type Props = {
+type PriceProps = {
   price: number;
-  id: number;
-  options?: { title: string; additionalPrice: number }[];
+  id: string;
 };
 
-const Price = ({ price, id, options }: Props) => {
+const Price = ({ price, id }: PriceProps) => {
   const [total, setTotal] = useState(price);
   const [quantity, setQuantity] = useState(1);
-  const [selected, setSelected] = useState(0);
 
   useEffect(() => {
-    setTotal(
-      quantity * (options ? price + options[selected].additionalPrice : price)
-    );
-  }, [quantity, selected, options, price]);
+    setTotal(quantity * price);
+  }, [quantity, price]);
 
   return (
     <div className="flex flex-col gap-4">
-      <h2 className="text-2xl font-bold">${total.toFixed(2)}</h2>
-      {/* OPTIONS CONTAINER */}
-      <div className="flex gap-4">
-        {options?.map((option, index) => (
-          <button
-            key={option.title}
-            className="min-w-[6rem] p-2 ring-1 ring-red-400 rounded-md"
-            style={{
-              background: selected === index ? "rgb(248 113 113)" : "white",
-              color: selected === index ? "white" : "red",
-            }}
-            onClick={() => setSelected(index)}
-          >
-            {option.title}
-          </button>
-        ))}
-      </div>
+      <h2 className="text-2xl font-bold text-red-500">${total.toFixed(2)}</h2>
       {/* QUANTITY AND ADD BUTTON CONTAINER */}
       <div className="flex justify-between items-center">
         {/* QUANTITY */}
@@ -47,13 +27,13 @@ const Price = ({ price, id, options }: Props) => {
             <button
               onClick={() => setQuantity((prev) => (prev > 1 ? prev - 1 : 1))}
             >
-              {"<"}
+              {"-"}
             </button>
             <span>{quantity}</span>
             <button
               onClick={() => setQuantity((prev) => (prev < 9 ? prev + 1 : 9))}
             >
-              {">"}
+              {"+"}
             </button>
           </div>
         </div>
