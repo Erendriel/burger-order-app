bc6a0ed66c878fe3dcd999074eb68df2233d8c03
Edgar Eli Perez-Figueroa y Larrañaga <edgar_eli12@hotmail.com>
2024-05-19 23:20:48 -0600

    fix: vercel errors

--- utils/auth.ts
+++ utils/auth.ts
@@ -1,51 +1,54 @@
-import { NextAuthOptions, User, getServerSession } from "next-auth";
-import GoogleProvider from "next-auth/providers/google"
-import { prisma } from "./connect";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { NextAuthOptions, User, getServerSession } from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { prisma } from "./connect";
 
 declare module "next-auth" {
-    interface Session{
+    interface Session {
         user: User & {
-            isAdmin: Boolean;
-        }
-    }
-}
-declare module "next-auth/jwt" {
-    interface JWT{
-            isAdmin: Boolean;
+            isAdmin: boolean;
+        };
     }
 }
 
-export const authOptions:NextAuthOptions ={
+declare module "next-auth/jwt" {
+    interface JWT {
+        isAdmin: boolean;
+    }
+}
+
+export const authOptions: NextAuthOptions = {
     adapter: PrismaAdapter(prisma),
     session: {
-        strategy:'jwt'
+        strategy: "jwt",
     },
-    providers:[
+    providers: [
         GoogleProvider({
-            // clientId: process.env.GOOGLE_ID as string,
-            // clientSecret: process.env.GOOGLE_SECRET as string,
             clientId: process.env.GOOGLE_ID!,
             clientSecret: process.env.GOOGLE_SECRET!,
         }),
     ],
-    callbacks:{
-        async session({token, session}){
-            if(token){
-                session.user.isAdmin = token.isAdmin
+    callbacks: {
+        async session({ token, session }) {
+            if (token) {
+                session.user.isAdmin = token.isAdmin;
             }
-            return session
+            return session;
         },
-        async jwt({token}){
-            const userInDb = await prisma.user.findUnique({
-                where:{
-                    email:token.email
-                }
-            })
-            token.isAdmin = userInDb?.isAdmin!;
+        async jwt({ token }) {
+            if (token.email) { 
+                const userInDb = await prisma.user.findUnique({
+                    where: {
+                        email: token.email,
+                    },
+                });
+                token.isAdmin = userInDb?.isAdmin ?? false;
+            } else {
+                token.isAdmin = false;
+            }
             return token;
-        }
-    }
-}
+        },
+    },
+};
 
-export const getAuthSession = ()=> getServerSession(authOptions)
+export const getAuthSession = () => getServerSession(authOptions);
